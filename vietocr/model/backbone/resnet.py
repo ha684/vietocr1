91bef3d4ead26cd7c094b08a410336f3fec60928
ha684 <157008172+ha684@users.noreply.github.com>
2024-08-12 12:11:00 +0700

    Update resnet.py

--- vietocr/model/backbone/resnet.py
+++ vietocr/model/backbone/resnet.py
@@ -38,7 +38,7 @@ class BasicBlock(nn.Module):
         return out
 class ResNet(nn.Module):
 
-    def __init__(self, input_channel, output_channel, block, layers):
+    def __init__(self, input_channel, output_channel, block, layers,pretrained):
         super(ResNet, self).__init__()
 
         self.output_channel_block = [int(output_channel / 16), int(output_channel / 8), int(output_channel / 4), int(output_channel / 2), output_channel]
