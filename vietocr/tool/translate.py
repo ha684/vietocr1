4087d1c86d0c087f70c2d641d9eaacd85428ed36
ha684 <robotpro682004@gmail.com>
2024-09-05 12:13:12 +0700

    fix error

--- vietocr/tool/translate.py
+++ vietocr/tool/translate.py
@@ -124,6 +124,7 @@ def build_model(config):
             config['cnn'], 
             config['transformer'],
             config['seq_modeling'])
+    model = torch.nn.DataParallel(model) 
     model = model.to(device)
 
     return model, vocab
