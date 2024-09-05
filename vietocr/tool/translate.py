5f0a7bdac78fd6c5814bc30f99cfc46a6c7a13a4
ha684 <robotpro682004@gmail.com>
2024-09-05 11:40:52 +0700

    Update translate.py

--- vietocr/tool/translate.py
+++ vietocr/tool/translate.py
@@ -115,7 +115,6 @@ def translate(img, model, max_seq_length=128, sos_token=1, eos_token=2):
     
     return translated_sentence, char_probs
 
-
 def build_model(config):
     vocab = Vocab(config['vocab'])
     device = config['device']
@@ -125,7 +124,7 @@ def build_model(config):
             config['cnn'], 
             config['transformer'],
             config['seq_modeling'])
-    
+    model = torch.nn.DataParallel(model) 
     model = model.to(device)
 
     return model, vocab
