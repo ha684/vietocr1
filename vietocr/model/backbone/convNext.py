e965d9e6921f520c3f96e1b3562dc03c9c19afb1
ha684 <robotpro682004@gmail.com>
2024-09-06 14:10:33 +0700

    Update convNext.py

--- vietocr/model/backbone/convNext.py
+++ vietocr/model/backbone/convNext.py
@@ -48,7 +48,7 @@ class ConvNeXtV2(nn.Module):
         for i in range(3):
             downsample_layer = nn.Sequential(
                     LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-                    nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2),
+                    nn.Conv2d(dims[i], dims[i + 1], kernel_size=ks[pool_idx], stride=ss[pool_idx]),
                     )
             self.downsample_layers.append(downsample_layer)
             pool_idx += 1
