a1b4c21298a022de55f94f70d06228d3c7922d3b
ha684 <robotpro682004@gmail.com>
2024-09-06 09:42:25 +0700

    Update convNext.py

--- vietocr/model/backbone/convNext.py
+++ vietocr/model/backbone/convNext.py
@@ -8,7 +8,7 @@ import timm
 class Block(nn.Module):
     def __init__(self, dim, drop_path=0.):
         super().__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=3, padding=1, groups=dim)
         self.norm = LayerNorm(dim, eps=1e-6)
         self.pwconv1 = nn.Linear(dim, 4 * dim)
         self.act = nn.GELU()
@@ -26,7 +26,6 @@ class Block(nn.Module):
         x = self.grn(x)
         x = self.pwconv2(x)
         x = x.permute(0, 3, 1, 2)
-        print(x.shape,input.shape)
         x = input + self.drop_path(x)
         return x
     
