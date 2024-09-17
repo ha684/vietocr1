c5784cb937540323eafe46695344d34fb882304c
ha684 <157008172+ha684@users.noreply.github.com>
2024-09-17 23:30:29 +0700

    Update trainer.py

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -107,7 +107,7 @@ class Trainer():
             self.model.parameters(),
             betas=(0.9, 0.98), 
             eps=1e-09,
-            weight_decay=0.001
+            weight_decay=0.0001
         )
         self.train_dataset_size = self._get_dataset_size(os.path.join(self.data_root,self.train_annotation))
         self.iterations_per_epoch = max(1, self.train_dataset_size // self.batch_size)
