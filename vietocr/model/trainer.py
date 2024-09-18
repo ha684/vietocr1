9f1a1c1bdec56462a13b31bd1ac69b8608f68ff4
ha684 <robotpro682004@gmail.com>
2024-09-18 09:46:58 +0700

    Update trainer.py

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -109,7 +109,7 @@ class Trainer:
             transforms = augmentor
 
         self.train_gen = self.data_gen(
-            "/kaggle/input/folder2/train_ha1".format(self.dataset_name),
+            "/kaggle/input/7mimages/merged_lmdb".format(self.dataset_name),
             self.data_root,
             self.train_annotation,
             self.masked_language_model,
@@ -129,7 +129,7 @@ class Trainer:
 
         if self.valid_annotation:
             self.valid_gen = self.data_gen(
-                "/kaggle/input/folder2/valid_ha1".format(self.dataset_name),
+                "/kaggle/input/valid-set/valid_set".format(self.dataset_name),
                 self.data_root,
                 self.valid_annotation,
                 masked_language_model=False,
